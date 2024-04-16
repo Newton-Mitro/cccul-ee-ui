1c59a46da48944e94ce98474e74edf2dd5781a83
Newton-Mitro <newtonmitro@gmail.com>
2024-04-16 14:16:44 +0600

    some

--- src/pages/exam-paper/ExamPaper.tsx
+++ src/pages/exam-paper/ExamPaper.tsx
@@ -2,20 +2,30 @@ import Loading from 'common/components/Loading';
 import AuthUserContext, {
   AuthUserContextType,
 } from 'common/context/AuthUserContext';
+import useCommand2 from 'common/hooks/useCommand2';
 import useQuery from 'common/hooks/useQuery';
-import { useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
+import Countdown from 'react-countdown';
+import { useNavigate } from 'react-router-dom';
+import ExamCompleted from './components/ExamCompleted';
 import ExamStepDetails from './components/ExamStepDetails';
 
 function ExamPaper() {
   const { authUser } = useContext(AuthUserContext) as AuthUserContextType;
+  const submitButtonRef = React.useRef<any>(null);
   const empExamId = Date.now();
+  const [activeStep, setActiveStep] = React.useState(1);
+  const navigate = useNavigate();
+
+  const handleSubmitClick = React.useCallback(
+    () => submitButtonRef.current?.click(),
+    []
+  );
 
   const {
     loading: currentExamDataLoading,
     data: currentExamData,
     setData: setCurrentExamData,
-    error: currentExamError,
-    setError: setCurrentExamError,
     executeQuery: executeGetCurrentExam,
   } = useQuery<any>();
 
@@ -23,8 +33,6 @@ function ExamPaper() {
     loading: sectionDataLoading,
     data: sectionData,
     setData: setSectionData,
-    error: sectionError,
-    setError: setSectionError,
     executeQuery: executeGetSectionData,
   } = useQuery<any>();
 
@@ -32,10 +40,12 @@ function ExamPaper() {
     loading: examCountDataLoading,
     data: examCountData,
     setData: setExamCountData,
-    error: examcountError,
-    setError: setExamCountError,
     executeQuery: executeGetExamCount,
   } = useQuery<any>();
+
+  const { data, setData, loading, executeCommand } = useCommand2<any>();
+
+  console.log(data);
 
   let question_for_sections = 0;
   if (sectionData) {
@@ -64,24 +74,132 @@ function ExamPaper() {
     );
   }, []);
 
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleSubmit = (optionsState: any) => {
+    console.log(optionsState);
+
+    let totalCorrectAns = 0;
+    optionsState?.forEach((element: any) => {
+      if (element) {
+        if (element.is_correct) {
+          totalCorrectAns = totalCorrectAns + 1;
+        }
+      }
+    });
+
+    const mappedValues = optionsState.map((option: any) => {
+      if (option !== undefined) {
+        return {
+          employee_code: authUser?.EmployeeCode,
+          employee_exam_id: empExamId,
+          exam_id: currentExamData?.id,
+          question_bank_id: option?.question_bank_id,
+          question_option_id: option?.id,
+          option_text: option?.option_text,
+          is_correct: option?.is_correct,
+        };
+      }
+    });
+
+    const application = {
+      id: empExamId,
+      emp_code_exam_id_exam_num: `${authUser?.EmployeeCode}_${currentExamData?.id}_${examCountData?.count}`,
+      exam_id: currentExamData?.id,
+      name: authUser?.UserName,
+      employee_code: authUser?.EmployeeCode,
+      exam_num: examCountData?.count,
+      total_questions: question_for_sections,
+      correct_answers: totalCorrectAns,
+      wrong_answers: question_for_sections - totalCorrectAns,
+      submitted_answers: mappedValues?.filter(
+        (option: any) => option !== undefined
+      ),
+    };
+
+    console.log(application);
+
+    executeCommand(
+      'http://10.77.77.22:8080/api/answer-sheets',
+      JSON.stringify(application),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+  };
+
   return (
     <div className="container mx-auto">
       <Loading
         isLoading={
-          currentExamDataLoading || sectionDataLoading || examCountDataLoading
+          currentExamDataLoading ||
+          sectionDataLoading ||
+          examCountDataLoading ||
+          loading
         }
       />
+
+      <ExamCompleted
+        isOpen={data ? true : false}
+        data={data}
+        handleClose={() => {
+          setData(null);
+          navigate('/auth/home');
+        }}
+      />
       <h1 className="pt-5 text-center text-3xl font-bold">Exam Paper</h1>
+      <div className="pb-4 text-center">
+        <h1 className="text-center text-xl">
+          {sectionData ? sectionData[activeStep - 1]?.section_title : ''}
+        </h1>
+        {sectionData &&
+          (activeStep % 2 === 0 ? (
+            <Countdown
+              date={Date.now() + sectionData[activeStep - 1]?.time_for_section}
+              className="text-2xl font-bold text-red-900"
+              onTick={(timeObj) => {
+                if (timeObj.total === 1000) {
+                  if (activeStep === 2) {
+                    handleSubmitClick();
+                  } else {
+                    handleNext();
+                  }
+                }
+              }}
+              onComplete={(obj) => {}}
+            />
+          ) : (
+            <Countdown
+              date={Date.now() + sectionData[activeStep - 1]?.time_for_section}
+              className="text-2xl font-bold text-red-900"
+              onTick={(timeObj) => {
+                if (timeObj.total === 1000) {
+                  if (activeStep === 2) {
+                    handleSubmitClick();
+                  } else {
+                    handleNext();
+                  }
+                }
+              }}
+              onComplete={(obj) => {}}
+            />
+          ))}
+      </div>
+
       {sectionData && authUser && examCountData && currentExamData && (
         <ExamStepDetails
+          handleSubmit={(optionsState: any) => {
+            handleSubmit(optionsState);
+          }}
+          handleNext={handleNext}
+          activeStep={activeStep}
           sections={sectionData}
           sectionLength={sectionData?.length}
-          employee_exam_id={empExamId}
-          emp_code_exam_id_exam_num={`${authUser?.EmployeeCode}_${currentExamData?.id}_${examCountData?.count}`}
-          exam_id={currentExamData?.id}
-          employee_code={authUser?.EmployeeCode}
-          exam_num={examCountData?.count}
-          total_questions={question_for_sections}
+          submitButtonRef={submitButtonRef}
         />
       )}
     </div>
--- src/pages/exam-paper/components/ExamStepDetails.tsx
+++ src/pages/exam-paper/components/ExamStepDetails.tsx
@@ -1,114 +1,33 @@
 import Loading from 'common/components/Loading';
-import AuthUserContext, {
-  AuthUserContextType,
-} from 'common/context/AuthUserContext';
-import useCommand2 from 'common/hooks/useCommand2';
 import useQuery from 'common/hooks/useQuery';
 import * as React from 'react';
-import Countdown from 'react-countdown';
-import { useNavigate } from 'react-router-dom';
-import ExamCompleted from './ExamCompleted';
+
 import ExamQuestions from './ExamQuestions';
 
 interface ExamStepDetailsProps {
   sections: any;
   sectionLength: number;
-  employee_exam_id: number;
-  emp_code_exam_id_exam_num: any;
-  exam_id: number;
-  employee_code: any;
-  exam_num: number;
-  total_questions: number;
+  handleNext: any;
+  activeStep: number;
+  handleSubmit: any;
+  submitButtonRef: any;
 }
 
 const ExamStepDetails: React.FC<ExamStepDetailsProps> = ({
   sections,
   sectionLength,
-  employee_exam_id,
-  emp_code_exam_id_exam_num,
-  exam_id,
-  employee_code,
-  exam_num,
-  total_questions,
+  handleNext,
+  activeStep,
+  handleSubmit,
+  submitButtonRef,
 }) => {
-  const [activeStep, setActiveStep] = React.useState(1);
-  const navigate = useNavigate();
-  const { authUser } = React.useContext(AuthUserContext) as AuthUserContextType;
-  const [optionsState, setOptionsState] = React.useState<any[]>([]);
-
-  const updateOptionsState = (selectedOption: any, index: number) => {
-    optionsState[index] = {
-      ...optionsState[index],
-      ...selectedOption,
-    };
-    setOptionsState([...optionsState]);
-  };
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const { data, status, setData, loading, error, setError, executeCommand } =
-    useCommand2<any>();
-
-  const handleSubmit = () => {
-    let totalCorrectAns = 0;
-    optionsState?.forEach((element: any) => {
-      if (element) {
-        if (element.is_correct) {
-          totalCorrectAns = totalCorrectAns + 1;
-        }
-      }
-    });
-
-    const mappedValues = optionsState.map((option: any) => {
-      if (option !== undefined) {
-        return {
-          employee_code: employee_code,
-          employee_exam_id: employee_exam_id,
-          exam_id: exam_id,
-          question_bank_id: option?.question_bank_id,
-          question_option_id: option?.id,
-          option_text: option?.option_text,
-          is_correct: option?.is_correct,
-        };
-      }
-    });
-
-    const application = {
-      id: employee_exam_id,
-      emp_code_exam_id_exam_num: emp_code_exam_id_exam_num,
-      exam_id: exam_id,
-      name: authUser?.UserName,
-      employee_code: employee_code,
-      exam_num: exam_num,
-      total_questions: total_questions,
-      correct_answers: totalCorrectAns,
-      wrong_answers: total_questions - totalCorrectAns,
-      submitted_answers: mappedValues.filter(
-        (option: any) => option !== undefined
-      ),
-    };
-
-    executeCommand(
-      'http://10.77.77.22:8080/api/answer-sheets',
-      JSON.stringify(application),
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-  };
-
   const {
     loading: questionsDataLoading,
     data: questionsData,
     setData: setQuestionsData,
-    error: questionsError,
-    setError: setQuestionsError,
     executeQuery: executeGetQuestionsData,
   } = useQuery<any>();
+  const [optionsState, setOptionsState] = React.useState<any[]>([]);
 
   React.useEffect(() => {
     if (activeStep !== 0 && sections.length > 0) {
@@ -121,54 +40,17 @@ const ExamStepDetails: React.FC<ExamStepDetailsProps> = ({
     }
   }, [activeStep, sections]);
 
+  const updateOptionsState = (selectedOption: any, index: number) => {
+    optionsState[index] = {
+      ...optionsState[index],
+      ...selectedOption,
+    };
+    setOptionsState([...optionsState]);
+  };
+
   return (
     <div className="flex flex-col">
-      <Loading isLoading={loading || questionsDataLoading} />
-      <ExamCompleted
-        isOpen={data ? true : false}
-        data={data}
-        handleClose={() => {
-          setData(null);
-          navigate('/auth/home');
-        }}
-      />
-      <div className="pb-4 text-center">
-        <h1 className="text-center text-xl">
-          {sections[activeStep - 1].section_title}
-        </h1>
-
-        {activeStep % 2 === 0 ? (
-          <Countdown
-            date={Date.now() + sections[activeStep - 1].time_for_section}
-            className="text-2xl font-bold text-red-900"
-            onTick={(timeObj) => {
-              if (timeObj.seconds === 1) {
-                if (activeStep === sectionLength) {
-                  handleSubmit();
-                } else {
-                  handleNext();
-                }
-              }
-            }}
-            onComplete={(obj) => {}}
-          />
-        ) : (
-          <Countdown
-            date={Date.now() + sections[activeStep - 1].time_for_section}
-            className="text-2xl font-bold text-red-900"
-            onTick={(timeObj) => {
-              if (timeObj.seconds === 1) {
-                if (activeStep === sectionLength) {
-                  handleSubmit();
-                } else {
-                  handleNext();
-                }
-              }
-            }}
-            onComplete={(obj) => {}}
-          />
-        )}
-      </div>
+      <Loading isLoading={questionsDataLoading} />
 
       {questionsData?.map((question: any, index: number) => {
         return (
@@ -182,10 +64,11 @@ const ExamStepDetails: React.FC<ExamStepDetailsProps> = ({
       })}
       <div className="mb-10 flex justify-center">
         <button
+          ref={submitButtonRef}
           className="mt-4 rounded-md bg-primary px-4 py-2 text-onPrimary hover:bg-primaryVariant hover:shadow"
           onClick={() => {
             if (activeStep === sectionLength) {
-              handleSubmit();
+              handleSubmit(optionsState);
             } else {
               handleNext();
             }
